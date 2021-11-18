2348a55403f6ac666aedbaadba06d475fbeb41be
marcelonader <marcelo.m.nader@gmail.com>
2021-11-18 18:09:26 -0300

    realiza ajustes de estilo

--- portfolio_react/src/components/SocialMedia/index.js
+++ portfolio_react/src/components/SocialMedia/index.js
@@ -2,7 +2,7 @@ import "./style.scss";
 
 const SocialMedia = () => (
   <div className="social-media-div">
-    <h1>Lets Talk</h1>
+    <h1>Let's Talk</h1>
     <div> 
       <a href="https://www.linkedin.com/in/marcelonader" target="_blank"><img src="https://img.shields.io/badge/-LinkedIn-%230077B5?style=for-the-badge&logo=linkedin&logoColor=white" target="_blank"/></a> 
       <a class='redesSociais' href="https://github.com/marcelonader" target="_blank"><img class='github' src="https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white" target="_blank"/></a> 
